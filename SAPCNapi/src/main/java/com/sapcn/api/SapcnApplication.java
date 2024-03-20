457cea0cc8f0c760b95c0b5f7b88e910a69247f2
Dhomyny Farias De Souza <dhomyryan@outlook.com>
2024-03-20 17:19:20 -0300

    criando as pastas do projeto

---
+++ SAPCNapi/src/main/java/com/sapcn/api/SapcnApplication.java
@@ -0,0 +1,13 @@
+package com.sapcn.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapcnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SapcnApplication.class, args);
+    }
+
+}
---
+++ SAPCNapi/src/test/java/com/sapcn/api/SapcnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapcn.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapcnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
